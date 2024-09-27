e9d2a121009e2b9f4471d4f41ed0de8291dc3d47
Fernando Ayats <ayatsfer@gmail.com>
2024-09-27 13:39:33 +0200

    add vimtrees

---
+++ nix/vimtrees/default.nix
@@ -0,0 +1,19 @@
+let
+  pkgs = import <nixpkgs> {};
+  inherit (pkgs) lib;
+
+  plugins = with pkgs.vimPlugins; [
+    nvim-treesitter.withAllGrammars
+    telescope-nvim
+  ];
+
+  foldPlugins = lib.foldl (
+    acc: next:
+      acc
+      ++ [
+        next
+      ]
+      ++ (foldPlugins (next.dependencies or []))
+  ) [];
+in
+  foldPlugins plugins
