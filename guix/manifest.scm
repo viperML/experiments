1af073bca99e8176e8ec888883c606f4aebdd651
Fernando Ayats <ayatsfer@gmail.com>
2024-12-06 14:59:16 +0100

    guix

---
+++ guix/manifest.scm
@@ -0,0 +1,7 @@
+(specifications->manifest (list "openmpi@4"
+                                "spdlog"
+                                "googletest"
+                                "cmake"
+                                "make"
+                                "gcc-toolchain"))
+
